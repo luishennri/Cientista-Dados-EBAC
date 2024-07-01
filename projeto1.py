0bf8d0cd516be0282d96fb1f196ffc5e69de10df
luishennri <luishenri60@gmail.com>
2024-07-01 13:25:51 -0300

    Adicionando o arquivo do projet1 no repositorio de python

---
+++ projeto1.py
@@ -0,0 +1,20 @@
+print("----------------")
+
+print("Hello World")
+
+print("----------------")
+
+x = int(input("Valor para x: "))
+y = (x**2) + (x*2) + 16.
+
+print(f"Resultado de ({x}**2) + ({x}*2) + 16 = {int(y)}")
+
+print("----------------")
+
+n1 = int(input("Valor para n1: "))
+n2 = int(input("Valor para n2: "))
+n3 = n1%n2
+
+print(f"O resto da divisão de {n1}/{n2} = {int(n3)}")
+
+print("----------------")
